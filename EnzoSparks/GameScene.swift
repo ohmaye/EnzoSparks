96a5f2769851b7b02e448241e0949eca64657902
Enio Ohmaye <enio.ohmaye@gmail.com>
2014-07-14 17:27:24 +0800

    1

--- EnzoSparks/GameScene.swift
+++ EnzoSparks/GameScene.swift
@@ -11,17 +11,19 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        /*
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
+        myLabel.text = "Enzo!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
+*/
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
+        /*
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
             
@@ -37,6 +39,7 @@ class GameScene: SKScene {
             
             self.addChild(sprite)
         }
+        */
     }
    
     override func update(currentTime: CFTimeInterval) {
--- EnzoSparks/GameViewController.swift
+++ EnzoSparks/GameViewController.swift
@@ -32,11 +32,12 @@ class GameViewController: UIViewController {
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+            //skView.showsFPS = true
+            //skView.showsNodeCount = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
+            scene.backgroundColor = SKColor.blackColor()
             
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
